7aacc3501a3087000e6700632b6b858a5fd1e712
Francisco Vilches <francisco.vilches@plexure.com>
2020-11-22 17:30:16 +1300

    Add project files.

---
+++ TodoInc/Server/Startup.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TodoInc.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder application, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                application
+                    .UseDeveloperExceptionPage()
+                    .UseWebAssemblyDebugging();
+            }
+            else
+            {
+                application
+                    .UseExceptionHandler("/Error")
+                    .UseHsts();
+            }
+
+            application
+                .UseHttpsRedirection()
+                .UseBlazorFrameworkFiles()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapRazorPages();
+                    endpoints.MapControllers();
+                    endpoints.MapFallbackToFile("index.html");
+                });
+        }
+    }
+}
